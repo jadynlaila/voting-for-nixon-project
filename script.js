d1a226eed061e84a0e4553331e78d99e42461550
Jadyn Calhoun <jaycal2013@gmail.com>
2021-02-02 15:26:18 -0700

    initial files, basic setup of launch and voting page

---
+++ script.js
@@ -0,0 +1,11 @@
+///no matter what at the end say thank you for viting for nixon
+//gross or weird pointer when hovering over the "wronG" optino
+//similar lading page to the parks and rec things
+//can do the same ending page for all "possible endings"
+///are you sure? you dont even wanna vote third party?
+//fill page with nixon buttons and small button for others
+///this could be the last page where i make the cursor invisible and it always makes them vote for nixon so they think it was their fault
+///have a button that says in big words "click here for free ___ or for $5000 and in small text below put and vote for nixon"
+//on same page but button that says "click here to take food from the hungry and in small text votefor __"
+///window event listener adds to entire page
+///logo probably american flag for voting
