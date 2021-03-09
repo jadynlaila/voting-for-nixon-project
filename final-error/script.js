8a6bae2c951184f5e094b54ea84dbdae84679a04
Jadyn Calhoun <jaycal2013@gmail.com>
2021-03-09 13:16:31 -0700

    changing file names for gitpages

--- final-error/script.js
+++ final-error/script.js
@@ -4,7 +4,7 @@ let spanLink = document.getElementById('link');
 link.addEventListener("click", startOver);
 
 function startOver() {
-    window.location.href = "../voting.html";
+    window.location.href = "../index.html";
 }
 
 
